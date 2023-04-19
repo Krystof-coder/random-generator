aaedcd5e09622917ef4097394eee0458f4506061
christechlab <118046754+christechlab@users.noreply.github.com>
2023-04-19 15:17:06 +0200

    random number generating
    with an animation

---
+++ javascript/random.js
@@ -0,0 +1,76 @@
+//! variables for random generating
+
+let numFrom;
+let numTo;
+let numOutput;
+let randomNumberOut;
+
+
+//# random number generating
+
+function randomNumber(params) {
+    numFrom = document.getElementById('num-from').value;
+    numTo = document.getElementById('num-to').value;
+
+    if (numFrom === "" || numTo === "") {
+        if (numFrom === "" && numTo !== "") {
+            numTo = parseFloat(numTo);
+            numFrom = numTo - 100;
+
+            getRandomNum(numFrom, numTo);
+            }
+        else if (numTo === "" && numFrom !== "") {
+            numFrom = parseFloat(numFrom);
+            numTo = numFrom + 100;
+
+            getRandomNum(numFrom, numTo);
+        }
+        else {
+            document.getElementById('numErrMes').innerHTML =
+            `<p>add at least one number!</p>`;
+        }
+    }
+    else {
+        numFrom = parseFloat(numFrom);
+        numTo = parseFloat(numTo)
+
+        getRandomNum(numFrom, numTo);
+    }
+}
+
+function getRandomNum(from, to) {
+    let int = null;
+    let turn = 1;
+    let ring = document.getElementById('ring');
+    let stop;
+
+    if ((from - to) > 10 || (from - to) < -10) {
+
+        stop = 20
+    }
+    else {
+        stop = 10;
+    }
+
+    clearInterval(int)
+
+    int = setInterval(gener, 100);
+        
+    ring.style.animationPlayState = "running";
+
+    function gener() {
+        if (turn == stop) {
+            document.getElementById('output').innerHTML =
+            Math.floor(Math.random() * (to - from + 1) + from);
+
+            ring.style.animationPlayState = "paused";
+            clearInterval(int)
+        } else {
+            turn++;
+
+            document.getElementById('output').innerHTML =
+            Math.floor(Math.random() * (to - from + 1) + from);
+        }
+    }
+
+    };
